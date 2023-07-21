764ec6299c2e468425969a18ff517ac47e2045ec
dryjorse <kozoevabdulazim@gmail.com>
2023-07-21 23:54:40 +0300

    new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,42 +5,48 @@ import projImg2 from "../assets/img/project-img2.jpg";
 import projImg3 from "../assets/img/project-img3.jpg";
 import projImg4 from "../assets/img/project-img4.jpg";
 import projImg5 from "../assets/img/project-img5.jpg";
+import projImg6 from "../assets/img/project-img6.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Gogorna",
-      description: "Html/Css & Js",
-      imgUrl: projImg1,
-      url: 'https://dryjorse.github.io/gogorna',
-    },
-    {
-      title: "Pokemons",
-      description: "React & Redux (without adaptive)",
-      imgUrl: projImg2,
-      url: 'https://dryjorse.github.io/pokemonSite',
-    },
-    {
-      title: "Webovio",
-      description: "Html/Css & Js",
-      imgUrl: projImg3,
-      url: 'https://dryjorse.github.io/webovio',
+      title: "Song-Kol Travel",
+      description: "React & Redux-toolkit & Typescript & Tailwind ",
+      imgUrl: projImg6,
+      url: "http://song-kol.com/",
     },
     {
       title: "Ani Reatsu",
       description: "React & Redux-toolkit & Node-js & Express & MongoDB ",
       imgUrl: projImg4,
-      url: 'https://defolinet.github.io/anireatsuFront',
+      url: "https://defolinet.github.io/anireatsuFront",
     },
     {
       title: "Select Sneakers",
       description: "React & Redux-toolkit & Node-js & Express & MongoDB ",
       imgUrl: projImg5,
-      url: 'https://dryjorse.github.io/selectSneakersFront',
+      url: "https://dryjorse.github.io/selectSneakersFront",
+    },
+    {
+      title: "Gogorna",
+      description: "Html/Css & Js",
+      imgUrl: projImg1,
+      url: "https://dryjorse.github.io/gogorna",
+    },
+    {
+      title: "Pokemons",
+      description: "React & Redux (without adaptive)",
+      imgUrl: projImg2,
+      url: "https://dryjorse.github.io/pokemonSite",
+    },
+    {
+      title: "Webovio",
+      description: "Html/Css & Js",
+      imgUrl: projImg3,
+      url: "https://dryjorse.github.io/webovio",
     },
   ];
 
@@ -50,50 +56,53 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2 className="projectsTitle">Projects</h2>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2 className="projectsTitle">Projects</h2>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="section"></Tab.Pane>
+                      <Tab.Pane eventKey="third"></Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
